24e3d989d0aaabdbb7b22ffe017c96fd72b58ee4
OliOSZ <146973447+OliOSZ@users.noreply.github.com>
2025-05-15 21:49:52 +0200

    name fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './gobal.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <p>Fire frontend</p>
   </StrictMode>,
 )
