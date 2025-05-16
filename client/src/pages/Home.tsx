59a2de66210e75975fa9a031b4f1b0731876fd4c
OliOSZ <146973447+OliOSZ@users.noreply.github.com>
2025-05-16 10:49:22 +0200

    Update tailwind working

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,11 @@ import GameCard from '../components/GameCard';
 
 export default function Home() {
   const games = [
-    { title: 'Tic Tac Toe', image: '/assets/tictactoe.png', link: '/TicTacToe' },
-    { title: 'Game 2', image: 'https://via.placeholder.com/250x160', link: '#' },
-    { title: 'Game 3', image: 'https://via.placeholder.com/250x160', link: '#' },
-    { title: 'Game 4', image: 'https://via.placeholder.com/250x160', link: '#' },
+    { title: 'Tic Tac Toe', image: '/tictactoe.png', link: '/TicTacToe' },
+    { title: 'Game 2', image: '/landscape-placeholder.svg', link: '#' },
+    { title: 'Game 3', image: '/landscape-placeholder.svg', link: '#' },
+    { title: 'Game 4', image: '/landscape-placeholder.svg', link: '#' },
+    { title: 'Game 5', image: '/landscape-placeholder.svg', link: '#' },
   ];
 
   return (
